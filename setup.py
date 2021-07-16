25190dbd93cc4a07cd92dc2d1bc3fdcc075e0b81
Theo Alves Da Costa <theo.alvesdacosta@ekimetrics.com>
2021-07-16 09:30:58 +0200

    Updated project to poetry requirements

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='carbonify',
-    packages=find_packages(),
-    version='0.0.0',
-    description='Open source library for carbon accounting and Lifecycle analysis',
-    author='Ekimetrics',
-)
